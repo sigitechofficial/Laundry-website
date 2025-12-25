2b736e8ff57f5cf8f2c9b980a1fa216bf0b83bb3
Hamza iqbal <sigidevelopers@gmail.com>
2025-12-25 15:28:01 +0500

    updated

--- utilities/URL.js
+++ utilities/URL.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "https://laundryb.theshippinghack.com/"
+export const BASE_URL = "https://prodlaundry.sigisolutions.net/"
 // export const BASE_URL = "http://192.168.1.139:3010/"
 export const googleApiKey = "AIzaSyD68_vw1gGE7LVVjJ5ZShy7qWwm9Rq0CBQ";
 export const libraries = ['places', 'geometry', 'drawing'];

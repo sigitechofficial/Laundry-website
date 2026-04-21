06ccb8c3ac2e2b8cea26afece9a1abce2c3088f2
hamzaiqbal01 <hamzii1133@gmail.com>
2026-04-21 15:36:21 +0500

    updated

--- utilities/URL.js
+++ utilities/URL.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "https://prodlaundry.sigisolutions.net/"
-// export const BASE_URL = "https://stagelaundry.sigisolutions.net/"
+// export const BASE_URL = "https://prodlaundry.sigisolutions.net/"
+export const BASE_URL = "https://stagelaundry.sigisolutions.net/"
 export const googleApiKey = "AIzaSyD68_vw1gGE7LVVjJ5ZShy7qWwm9Rq0CBQ";
 export const libraries = ['places', 'geometry', 'drawing'];

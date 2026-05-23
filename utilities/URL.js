43efbb4eadce49f0bcf48c2ee6d0428e722b6711
hamzaiqbal01 <hamzii1133@gmail.com>
2026-05-23 15:02:26 +0500

    Switch BASE_URL to production environment and comment out local development URL

--- utilities/URL.js
+++ utilities/URL.js
@@ -1,5 +1,5 @@
-// export const BASE_URL = "https://prodlaundry.sigisolutions.net/"
-export const BASE_URL = "http://localhost:8083/"
+export const BASE_URL = "https://prodlaundry.sigisolutions.net/"
+// export const BASE_URL = "http://localhost:8083/"
 // export const BASE_URL = "https://stagelaundry.sigisolutions.net/"
 export const googleApiKey = "AIzaSyD68_vw1gGE7LVVjJ5ZShy7qWwm9Rq0CBQ";
 export const libraries = ['places', 'geometry', 'drawing'];
